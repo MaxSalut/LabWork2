53518774ae6a19fd9bdcfd1f69dd85c036d5b55e
MaxSalut <m.bezverkhyi28@gmail.com>
2024-11-13 20:02:38 +0200

    Edit GUI

--- LabWork2/MainPage.xaml.cs
+++ LabWork2/MainPage.xaml.cs
@@ -15,28 +15,9 @@ namespace LabWork2.Views
         public MainPage()
         {
             InitializeComponent();
-            xmlData = new List<Student>();
-            xmlDoc = ConvertToXDocument(xmlData); // Ініціалізація xmlDoc в конструкторі
         }
 
-        private XDocument ConvertToXDocument(List<Student> students)
-        {
-            return new XDocument(
-                new XElement("Students",
-                    students.Select(student =>
-                        new XElement("Student",
-                            new XElement("FullName", student.FullName),
-                            new XElement("Faculty", student.Faculty),
-                            new XElement("Department", student.Department),
-                            new XElement("Course", student.Course),
-                            new XElement("Room", student.Room),
-                            new XElement("CheckIn", student.CheckIn),
-                            new XElement("CheckOut", student.CheckOut)
-                        )
-                    )
-                )
-            );
-        }
+        
 
         public async Task HandleCloseRequestAsync()
         {
@@ -54,27 +35,11 @@ namespace LabWork2.Views
 
         private void OnClearClicked(object sender, EventArgs e)
         {
-            NameEntry.Text = FacultyEntry.Text = CourseEntry.Text = DateEntry.Text = string.Empty;
+            NameEntry.Text = FacultyEntry.Text = CourseEntry.Text = RoomEntry.Text = string.Empty;
         }
         private async void OnTransformToHtmlClicked(object sender, EventArgs e)
         {
-            HtmlTransformer transformer = new HtmlTransformer();
-            var htmlContent = transformer.TransformToHtml(xmlDoc); // Передаємо xmlData
-
-            // Шлях до файлу, куди зберігати HTML
-            string projectDirectory = Directory.GetCurrentDirectory();
-            string filePath = Path.Combine(projectDirectory, "output.html");
-
-            // Записуємо в файл
-            try
-            {
-                await File.WriteAllTextAsync(filePath, htmlContent);
-                await DisplayAlert("Успіх", $"HTML файл збережено за адресою: {filePath}", "OK");
-            }
-            catch (Exception ex)
-            {
-                await DisplayAlert("Помилка", $"Не вдалося зберегти файл: {ex.Message}", "OK");
-            }
+            await DisplayAlert("Результат", "Виконано!", "OK");
         }
 
     }
